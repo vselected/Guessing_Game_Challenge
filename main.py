94b1a4eef263b94d1da1f4f1459753aea35b9519
vselected <dominik.sarleja@gmail.com>
2024-11-16 14:35:55 +0100

    Print introduction to the game and explain the rules

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import random
 # Create a variable that will hold random number
 random_number = random.randint(1,100)
 
+# Print introduction to the game and explain the rules
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
 
